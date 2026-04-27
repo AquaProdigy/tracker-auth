7bd934f40306e1a062f68ad98e2ebfc5153e05a4
daniil <suprychev228338@gmail.com>
2026-04-27 15:23:25 +0300

    changed response status for invalid password

--- src/main/java/org/example/trackerauth/advice/GlobalExceptionHandler.java
+++ src/main/java/org/example/trackerauth/advice/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.example.trackerauth.exceptions.EmailAlreadyExistsException;
 import org.example.trackerauth.exceptions.InvalidPasswordException;
 import org.example.trackerauth.exceptions.UserNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,14 +15,14 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(UserNotFoundException.class)
+    @ExceptionHandler({UserNotFoundException.class, UsernameNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorMessage handleUserNotFoundException(UserNotFoundException ex) {
         return new ErrorMessage(ex.getMessage());
     }
 
     @ExceptionHandler(InvalidPasswordException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public ErrorMessage handleInvalidPasswordException(InvalidPasswordException ex) {
         return new ErrorMessage(ex.getMessage());
     }
