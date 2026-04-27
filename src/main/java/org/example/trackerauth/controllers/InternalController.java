8f100044e0d865f735b2ba78b13fab6293dc545b
daniil <suprychev228338@gmail.com>
2026-04-27 15:02:09 +0300

    added use check internal api key in internal controller

--- src/main/java/org/example/trackerauth/controllers/InternalController.java
+++ src/main/java/org/example/trackerauth/controllers/InternalController.java
@@ -5,8 +5,10 @@ import org.example.trackerauth.dto.UserDto;
 import org.example.trackerauth.mapper.UserMapper;
 import org.example.trackerauth.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,17 +18,21 @@ import java.util.List;
 @RequestMapping("/internal/users")
 @RequiredArgsConstructor
 public class InternalController {
+    private static final String internalNameHeader = "X-Internal-Api-Key";
     private final UserRepository userRepository;
     private final UserMapper userMapper;
-
-    @Value("${internal.name-header}")
-    private String internalNameHeader;
 
     @Value("${internal.api-key}")
     private String internalApiKey;
 
     @GetMapping
-    public ResponseEntity<List<UserDto>> getAllUsers() {
+    public ResponseEntity<List<UserDto>> getAllUsers(
+            @RequestHeader(internalNameHeader) String key
+    ) {
+        if (!key.equals(internalApiKey)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
         return ResponseEntity.ok().header(internalNameHeader, internalApiKey)
                 .body(
                         userRepository.findAll()
